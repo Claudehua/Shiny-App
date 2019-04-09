878b2d242076c9336e146e48e866ad29b885336a
Shiwei Hua <sh3804>
2019-04-09 17:11:01 -0400

    Shiny app version 1
    Shiny app version 1

---
+++ README.md
@@ -0,0 +1,53 @@
+# Project 2: Shiny App Development Version 2.0
+
+### [Project Description](doc/project2_desc.md)
+
+![screenshot](output/screenshot.jpeg)
+
+In this second project of GR5243 Applied Data Science, we develop a version 2.0 of an *Exploratory Data Analysis and Visualization* shiny app on a topic of your choice using [NYC Open Data](https://opendata.cityofnewyork.us/) or U.S. government open data released on the [data.gov](https://data.gov/) website. See [Project 2 Description](doc/project2_desc.md) for more details.  
+
+The **learning goals** for this project is:
+
+- business intelligence for data science
+- study legacy codes and further development
+- data cleaning
+- data visualization
+- systems development/design life cycle
+- shiny app/shiny server
+
+*The above general statement about project 2 can be removed once you are finished with your project. It is optional.
+
+## Project Title: 2018 NYC Yellow Taxi 2.0: For Drivers
+Term: Spring 2019
+
++ Team #9
++ **2018 NYC Yellow Taxi 2.0: For Drivers**:
+	+ Ziyi Liao
+	+ Shengjie Sun
+	+ Haiqing Xu
+	+ Xinzhu Wang
+	+ Shiwei Hua
+
++ **Project summary**: 
+
+The **Shiny App 2018 NYC Yellow Taxi 2.0: For Drivers** is an updated version of the Open Data App. Our updated app is designed specifically for taxi drivers’ reference. 
+
+In this project, we updated the data to the 2018 yellow taxi rides in New York City and analyzed 63.4-million records. Instead of using neighborhood zones, we restructured to NYC TLC taxi zones for better user experiences. Besides updating color-codes in the density of taxi rides, fare per distance, and distinctions between business and non-business days, we added tips percentages and top-three popular destinations to provide better visual representation and references for drivers.
+
+Generally, this release improved overall app performance for taxi drivers.
+
++ **Contribution statement**: ([default](doc/a_note_on_contributions.md)) All team members contributed equally in all stages of this project. All team members approve our work presented in this GitHub repository including this contributions statement. 
+
+Following [suggestions](http://nicercode.github.io/blog/2013-04-05-projects/) by [RICH FITZJOHN](http://nicercode.github.io/about/#Team) (@richfitz). This folder is orgarnized as follows.
+
+```
+proj/
+├── app/
+├── lib/
+├── data/
+├── doc/
+└── output/
+```
+
+Please see each subfolder for a README file.
+
---
+++ app/ui.r
@@ -0,0 +1,96 @@
+library(leaflet)
+  navbarPage("NYC Yellow Taxi", id = "navigation",
+             tabPanel("Statistical Map",
+                      div(class="outer",
+                          
+                          tags$head(
+                            #change the layout formats
+                            includeCSS("styles.css"),
+                            includeScript("gomap.js")
+                          ),
+                          
+                          leafletOutput("map", width="100%", height="100%"),
+                          
+                          fixedPanel(id = "controls", class = "panel panel-default", 
+                                     draggable = T, top = 60, right = 5,
+                                     width = 160, height = 130,
+                                     
+                                     radioButtons("layers", label = h4("Layers"),
+                                                  choices = list("Pick Up Numbers" = "count", "Tips" = "tips"), 
+                                                  selected = "count")
+                          ),
+                          fixedPanel(id = "controls", class = "panel panel-default",
+                                     draggable = TRUE, top = 60, left = 40,
+                                     width = 330, height = "auto",
+                                     h3("Panel"),
+                                     
+                                     
+                                     selectInput("days", h4("Days"), c("All Day", "Business Day", "Non-business Day"),selected = "All Day"),
+                                     
+                                     checkboxInput(inputId = "showhr",
+                                                   label = strong("Show hours"),
+                                                   value = FALSE),
+                                     
+                                     conditionalPanel(condition = "input.showhr == false"
+                                                      
+                                     ),
+                                     
+                                     
+                                     conditionalPanel(condition = "input.showhr == true",
+                                                      sliderInput(inputId = "hr_adjust",
+                                                                  label = "Choose the time of the day:",
+                                                                  min = 0, max = 23, value = NULL, step = 1)
+                                     )
+                                     
+                          )
+                      )
+             ),
+             
+             tabPanel("Interactive Map",
+                      div(class="outer",
+                          
+                          tags$head(
+                            #change the layout formats
+                            includeCSS("styles.css"),
+                            includeScript("gomap.js")
+                          ),
+                          
+                          leafletOutput("Imap", width="100%", height="100%"),
+                          
+                          fixedPanel(id = "controls", class = "panel panel-default", 
+                                     draggable = T, top = 60, right = 5,
+                                     width = 160, height = 130,
+                                     
+                                     radioButtons("Ilayers", label = h4("Layers"),
+                                                  choices = list("Pick Up Numbers" = "count"), 
+                                                  selected = "count")
+                          ),
+                          fixedPanel(id = "controls", class = "panel panel-default",
+                                     draggable = TRUE, top = 60, left = 40,
+                                     width = 330, height = "auto",
+                                     h3("Panel"),
+                                     
+                                     
+                                     selectInput("Idays", h4("Days"), c("Business Day", "Non-business Day"),selected = "Business Day"),
+                                     
+                                     checkboxInput(inputId = "Ishowhr",
+                                                   label = strong("Show hours"),
+                                                   value = FALSE),
+                                     
+                                     conditionalPanel(condition = "input.Ishowhr == false"
+                                                      
+                                     ),
+                                     
+                                     
+                                     conditionalPanel(condition = "input.Ishowhr == true",
+                                                      sliderInput(inputId = "Ihr_adjust",
+                                                                  label = "Choose the time of the day:",
+                                                                  min = 0, max = 23, value = NULL, step = 1)
+
+  
+                                     )
+                                     
+                          )
+                 )
+         )
+)
